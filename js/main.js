a8825ea92b4cd1c9107a1e0383125043e74af4a4
Kaan Sato <kaansato41@gmail.com>
2024-07-18 14:38:11 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const modelViewer = document.querySelector('#reveal');
+
+    modelViewer.addEventListener('load', () => {
+
+        console.log('Loaded !');
+
+        /*
+        const scene = modelViewer.scene;
+        const model = scene.model;
+
+        if (model) {
+            const meshes = model.children;
+            for (let i = 0; i < meshes.length; i++) {
+                const mesh = meshes[i];
+
+                if (mesh.morphTargetInfluences) {
+                    // Example: Set the first blendshape/morph target to 50%
+                    mesh.morphTargetInfluences[0] = 0.5;
+                }
+            }
+        }
+        */
+    });
+});
+
 window.setViseme = function(viseme) {
     window.Android.log('Viseme changed to: ' + viseme);
 }
